b0ff508fb7d7f48b0a993edcb72706191f9a8740
asus TX <2653644677@qq.com>
2022-06-28 20:03:06 +0800

    新增抖音视频爬虫

--- douyin_video_spider/README.md
+++ douyin_video_spider/README.md
@@ -2,6 +2,15 @@
  - 随便拿一位抖音作者当案例：[https://www.douyin.com/user/MS4wLjABAAAAkvysSgdqmkgtgucxkirpMWFHbTeZgVOW7zcdUjU3jM4](https://www.douyin.com/user/MS4wLjABAAAAkvysSgdqmkgtgucxkirpMWFHbTeZgVOW7zcdUjU3jM4)
 
  - 输入抖音的作者主页链接，即可自动下载保存其所有发布的视频
+	 - 保存形式为：以作者昵称为文件夹名，以视频标题为视频名称保存
+
+ - 其中保存视频的同时，还会保存视频的相关信息(csv格式)：
+	 - 视频链接
+	 - 视频标题
+	 - 视频点赞数
+	 - 视频评论数
+	 - 视频收藏数
+	 - 视频发布日期
 
  - 该爬虫使用到的模块：
 	 - re
--- douyin_video_spider/run_spider.py
+++ douyin_video_spider/run_spider.py
@@ -14,27 +14,36 @@ class DownloadVideo:
     def __init__(self, url):
         self.url = url
         self.headers = {
-            "cookie": "douyin.com; odin_tt=1b01bcb8d90716d55f94fd5f6db002e5ece540ab2504c8aad2b5c37e03e4aa5567cc13e046ae77344f381490bc9c1d21d95ebec9daf6865b7c654200790fe6a2; ttcid=c92e805224854c19b5e889b9e383e53777; tt_scid=KhTWnuh3idwWm52BSYmLKO85gjugzBkadfxJGESQL2QipMxm76dP7UA2feWh-SPwad73; ttwid=1%7CmDw1LRv-hGPYqQxDacf59SVVc2wZXfToFhcp93NZ2I4%7C1651748569%7C7ad913b4c829af89cb79aa6a3a718f91601cfb083d2bfdb5678b150f4b0d7ec2; passport_csrf_token=b55d814f0b5a8949554209ce20e7dc2b; passport_csrf_token_default=b55d814f0b5a8949554209ce20e7dc2b; s_v_web_id=verify_l2swgdft_jrNsAzY8_CQFp_4qqo_BFvR_jQUKYZmgJmyr; _tea_utm_cache_1300=undefined; douyin.com; strategyABtestKey=1656377462.724; AB_LOGIN_GUIDE_TIMESTAMP=%221656377462532%22; pwa_guide_count=%223%22; THEME_STAY_TIME=%22299511%22; IS_HIDE_THEME_CHANGE=%221%22; _tea_utm_cache_2018=undefined; __ac_signature=_02B4Z6wo00f01M5iVQgAAIDBRSnO6jsm5TzORlGAAFEnatBh5Ap4ZVHVKmxPfyEsJzRZnSWLQj2BzUaK.eu57.agZqr9HEBFR.Ne-qDV81SKwv5RopUJJt-r71QK.8J35QLYWK3xNxEssDg3bc; __ac_nonce=062bab5b100587fc9da87; msToken=MJe-pQK_3-whoNZhStDkPrundY1m5_VxnD3Fq5r7uqAdQH9UmM6ywGHDvlCusCoLmYD1_5Ck2HQxNLZ2-Q2ITHtLo5LJRfrNXtR_OVNTpmyqAd0Q2kaq5LCLKC5G6aqawA==; home_can_add_dy_2_desktop=%221%22; msToken=9Fg3VeHdJJ4UYgMDCRdukBZyj_ikEd0V_2EzCsQLy-Ddhq2dEBf2SsiF7ksHm2XDaZMZP0WNJWWvYLJL8XcRhor4P6DZuKhJxBz8yJFXHZrp8-DwqAKcSUqc4B5_pZGHOQ==",
+            # 记得补充一下自己的cookie值
+            "cookie": "",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36 Edg/103.0.1264.37"
         }
-        self.driver = webdriver.Chrome()
+        # 使用无头模式
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('-–disable-gpu')
+        self.driver = webdriver.Chrome(options=options)
 
     def run(self):
         self.driver.get(self.url)
         # 将滚轮滑到最底部，从而加载所有视频
-        self.move_pulley()
+        # self.move_pulley()
         # 作者昵称
         author_name = self.driver.find_elements_by_xpath('//span[@class="Nu66P_ba"]')[0].text
         print(author_name)
         # 以作者名称创建文件夹
-        os.mkdir(author_name)
+        try:
+            os.mkdir(author_name)
+        except FileExistsError:
+            print(f"{author_name} 文件夹已存在！")
         li_list = self.driver.find_elements_by_xpath('//li[@class="ECMy_Zdt"]')
         for li in li_list:
             video_link = li.find_element_by_xpath('./a').get_attribute('href')
             print("正在保存 --- ", video_link)
-            video_url, video_title = self.get_video_url(video_link)
-            self.save_video(video_url, video_title, author_name)
+            item, video_url = self.get_video_url(video_link)
+            self.save_video(item, video_url, author_name)
             time.sleep(3)
+            break
 
     def move_pulley(self):
         temp_height = 0
@@ -55,18 +64,39 @@ class DownloadVideo:
         res = requests.get(video_link, headers=self.headers).text
         # 对返回的源码进行url解码
         unquote_res = requests.utils.unquote(res)
-        query_link = re.findall('"src":"(.*?)"},', unquote_res)[0]
-        # 视频链接
-        video_url = "https:" + query_link
-        # 视频标题
-        video_title = re.findall('<title.*>(.*?)</title>', unquote_res)[0]
-        return video_url, video_title
+        # 原视频链接，用于下载保存
+        video_url = "https:" + re.findall('"src":"(.*?)"},', unquote_res)[0]
+        # 获取视频其他信息
+        item = self.get_other_data(unquote_res, video_link)
+        return item, video_url
 
-    def save_video(self, video_url, video_title, author_name):
-        with open(f'{author_name}/{video_title}.mp4', 'wb') as w:
+    def get_other_data(self, unquote_res, video_link):
+        other_data = re.findall('<span class="CE7XkkTw">(.*?)</span>', unquote_res)
+        item = [
+            # 视频链接
+            video_link,
+            # 视频标题
+            re.findall('<title.*>(.*?)</title>', unquote_res)[0],
+            # 视频点赞数
+            other_data[0],
+            # 视频评论数
+            other_data[1],
+            # 视频收藏数
+            other_data[2],
+            # 视频的发布日期
+            re.findall('<span class="aQoncqRg">(.*?)</span>', unquote_res)[0].split('>')[-1]
+        ]
+        return item
+
+    def save_video(self, item, video_url, author_name):
+        with open(f'{author_name}/{item[1]}.mp4', 'wb') as w:
             res = requests.get(video_url).content
             w.write(res)
-            print(video_title, " --- 保存完成！")
+            print(item[1], " --- 保存完成！")
+        with open(f'{author_name}/{author_name}_所有视频信息.csv', 'a+') as a:
+            if a.read() == '':
+                a.write(f'视频链接,视频标题,点赞数,评论数,收藏数,发布日期\n')
+            a.write(','.join(item) + '\n')
 
 
 if __name__ == '__main__':
