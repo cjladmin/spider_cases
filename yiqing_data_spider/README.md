68fe8763738066b07aa4ebafb94c3efd9e0fae96
asus TX <2653644677@qq.com>
2022-07-04 17:57:24 +0800

    更新城市疫情信息爬虫的API接口

--- yiqing_data_spider/README.md
+++ yiqing_data_spider/README.md
@@ -1,5 +1,5 @@
 ## <center>✨获取城市实时疫情数据信息✨</center>
- - 腾讯疫情数据：[https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5](https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5)
+ - 腾讯疫情数据API：[https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=diseaseh5Shelf](https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=diseaseh5Shelf)
 
  - 输入城市名称获取疫情数据，包含：
     - 最近更新日期
@@ -9,5 +9,5 @@
     - 累计治愈人数
     - 累计死亡人数
  - 该爬虫使用到的模块：
-	 - re
+	 - json
 	 - requests
--- yiqing_data_spider/run_spider.py
+++ yiqing_data_spider/run_spider.py
@@ -1,49 +1,73 @@
 # encoding: utf-8
 # @Time : 2022/5/3 23:23
 # @Author : Torres-圣君
-# @File : douban_run_spider.py
-# @Sofaware : PyCharm
-import re
+# @File : run_spider.py
+# @Software : PyCharm
+import json
 import requests
 
-url = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5"
+url = "https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=diseaseh5Shelf"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36 Edg/103.0.1264.44"
+}
 
 
 def run(city):
-    res = requests.get(url)
-    data = res.text
-    yiqing_data = data.replace('"', "").replace('\\', "")
-    result = re.findall(u'[\u4e00-\u9fa5]+', yiqing_data)
-    if city in result:
-        # 最近更新日期 lastUpdateTime
-        lastUpdateTime = f'<{re.findall(r"lastUpdateTime:(.*),chinaTotal", yiqing_data)[0]}>'
-
-        # 新增确诊人数 today_confirm
-        today_confirm = "新增确诊：" + re.findall(f"{city}" + r".*?,today:{confirm:(\d+),", yiqing_data)[0] + "人"
-
-        # 目前确诊人数 total_nowConfirm
-        total_nowConfirm = "目前确诊：" + re.findall(f'{city}' + r".*?nowConfirm:(\d+),", yiqing_data)[0] + "人"
-
-        # 累计确诊人数 total_confirm
-        total_confirm = f"累计确诊：" + re.findall(rf"{city}.*?total:.*?confirm:(\d+),", yiqing_data)[0] + "人"
-
-        # 累计治愈人数 total_heal
-        total_heal = "累计治愈：" + re.findall(f'{city}' + r".*?heal:(\d+),", yiqing_data)[0] + "人"
-
-        # 累计死亡人数 total_dead
-        total_dead = "累计死亡：" + re.findall(f'{city}' + r".*?dead:(\d+),", yiqing_data)[0] + "人"
-
-        return f"{city}疫情更新日期：\n" \
-               f"{lastUpdateTime}\n" \
-               f"————————————————————————\n" \
-               f"该地区疫情数据如下：\n" \
-               f"{today_confirm}\n" \
-               f"{total_nowConfirm}\n" \
-               f"{total_confirm}\n" \
-               f"{total_heal}\n" \
-               f"{total_dead}"
+    res = requests.get(url, headers=headers).json()
+    all_data = res['data']['diseaseh5Shelf']
+    last_update_time = all_data["lastUpdateTime"]
+    # 保存城市列表
+    save_city_list(all_data)
+    # 读取城市名称列表
+    city_list = json.loads(open("city_list.json", encoding='utf-8').read())
+    city = city.strip("省市")
+    if city == "中国":
+        data_ = all_data["areaTree"][0]
     else:
-        return f"没有查询到{city}的疫情数据~"
+        try:
+            if city in city_list["省"]:
+                # 提取当前省份的所有数据
+                data_ = [x for x in all_data["areaTree"][0]["children"] if x["name"] == city][0]
+            elif city in city_list["市"]:
+                # 提取当前城市的所有数据
+                data_ = [y for x in all_data["areaTree"][0]["children"] for y in x["children"] if y["name"] == city][0]
+            else:
+                return f"没有查询到{city}的疫情数据~"
+        except IndexError:
+            return "疫情接口出现异常，请稍后重试~"
+    confirm = data_["total"]["confirm"]          # 累计确诊
+    heal = data_["total"]["heal"]                # 累计治愈
+    dead = data_["total"]["dead"]                # 累计死亡
+    now_confirm = data_["total"]["nowConfirm"]   # 目前确诊
+    add_confirm = data_["today"]["confirm"]      # 新增确诊
+    return f"{city}疫情更新日期：\n" \
+           f"{last_update_time}\n" \
+           f"————————————————————————\n" \
+           f"该地区疫情数据如下：\n" \
+           f"新增确诊：{add_confirm}\n" \
+           f"目前确诊：{now_confirm}\n" \
+           f"累计确诊：{confirm}\n" \
+           f"累计治愈：{heal}\n" \
+           f"累计死亡：{dead}"
+
+
+def save_city_list(all_data):
+    with open("city_list.json", 'w', encoding='utf-8') as w:
+        # 保存所有省份和城市名称
+        sheng_list = []
+        shi_list = []
+        for i in all_data["areaTree"][0]["children"]:
+            sheng_list.append(i["name"])
+            sheng_list.append(i["name"]+"省")
+            for j in i["children"]:
+                shi_list.append(j["name"])
+                shi_list.append(j["name"]+"市")
+        dict_city = {
+            "省": sheng_list,
+            "市": shi_list
+        }
+        w.write(json.dumps(dict_city, indent=1, ensure_ascii=False))
+        print("城市列表保存完成！")
 
 
 city_name = input("请输入要查的城市名：")
